dc3983ef46c6b434b7b79a371fc744bfad6ea105
Sajal Mishra <sajalmishra44@gmail.com>
2021-01-04 01:03:12 +0530

    implment add for step - 1

--- src/main/java/sajal/App.java
+++ src/main/java/sajal/App.java
@@ -6,8 +6,28 @@ package sajal;
  */
 public class App 
 {
+   
+
+    public int Add(String str){
+        if (str == "") return 0;
+        else{
+            String num[]= str.split(",");
+            if(num.length==1)
+                return Integer.parseInt(num[0]);
+            else
+                return Integer.parseInt(num[0]) + Integer.parseInt(num[1]);
+            
+        }
+    }
+
+
+
+
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App app = new App();
+
+        System.out.println( "Hello World!  " + app.Add("2,3") );
     }
 }
