e4a67b8994520586e3fac6dc0016baa05a8dfdb8
Sajal Mishra <sajalmishra44@gmail.com>
2021-01-04 00:46:05 +0530

    created test for  step - 1

--- src/test/java/sajal/AppTest.java
+++ src/test/java/sajal/AppTest.java
@@ -1,20 +1,42 @@
 package sajal;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.Before;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+    private App app;
     /**
      * Rigorous Test :-)
      */
+    // @Test
+    // public void shouldAnswerWithTrue()
+    // {
+    //     assertTrue( true );
+    // }
+
+    @Before
+    public void setUp(){
+         app = new App();
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testEmptyString(){
+        assertEquals(0,app.Add(""));
+    }
+
+    @Test 
+    public void testOne(){
+        assertEquals(5, app.Add("5"));
+    }
+
+    @Test 
+    public void testTwo(){
+        assertEquals(5, app.Add("2,3"));
     }
 }
